c296e405ec3dd69ef523e7c6d3522318b4fbf795
Balbisaur <albi3479@yahoo.com>
2024-04-29 07:14:52 -0500

    first commit

---
+++ Name_Validation.py
@@ -0,0 +1,12 @@
+import re
+names = ["Abraham Lincoln", "Andrew P Garfield", "peter pan", "Connor Milliken", "Jordan Alexander Williams", "Madonna", "programming is cool"]
+
+def verify_names(names):
+    name_verification = r"^[A-Z][a-z]*(?: [A-Z][a-z]*)"
+    for name in names:
+        if re.match(name_verification, name):
+            print(name)
+        else:
+            print('invalid name')
+        
+verify_names(names)
