d86b8ff8945bc396982283103a9d5edc031d30ce
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-04-29 07:16:06 -0500

    Update Name_Validation.py

--- Name_Validation.py
+++ Name_Validation.py
@@ -2,7 +2,7 @@ import re
 names = ["Abraham Lincoln", "Andrew P Garfield", "peter pan", "Connor Milliken", "Jordan Alexander Williams", "Madonna", "programming is cool"]
 
 def verify_names(names):
-    name_verification = r"^[A-Z][a-z]*(?: [A-Z][a-z]*)"
+    name_verification = r"[A-Z][a-z]*(?: [A-Z][a-z]*)"
     for name in names:
         if re.match(name_verification, name):
             print(name)
